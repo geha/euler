64e6d14d015e3bf1d55a76a3ef50b82961f34e75
Georg Habeck <geha00@gmail.com>
2011-12-11 05:37:53 +0100

    solve problems 31 & 56

---
+++ problem_31/problem_31.rb
@@ -0,0 +1,19 @@
+counter = 0
+
+200.step(0, -200) do |a|
+	a.step(0, -100) do |b|
+		b.step(0, -50) do |c|
+			c.step(0, -20) do |d|
+				d.step(0, -10) do |e|
+					e.step(0, -5) do |f|
+						f.step(0, -2) do |g|
+							counter += 1
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+puts counter
---
+++ problem_56/problem_56.rb
@@ -0,0 +1,10 @@
+max = 0
+
+for a in 1..99
+	for b in 1..99
+		c = a**b
+		max = [c.to_s.chars.inject(0) {|sum, digit| sum + digit.to_i}, max].max
+	end
+end
+
+puts max
