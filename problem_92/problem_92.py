0543133a540521ae163a5f16022bdc4f6fa7d88a
Georg Habeck <geha00@gmail.com>
2014-04-06 14:27:36 +0200

    tweak problem 92 a little more

--- problem_92/problem_92.py
+++ problem_92/problem_92.py
@@ -4,23 +4,22 @@ def square_sum(number):
 	return sum([int(x)**2 for x in str(number)])
 
 count = 0
-table = [None]*568
+square_set = set()
 
+# calculate all possible sum for numbers 1 to 10**7
 for i in range (1, 568):
 	number = i
 	while True:
 		if number == 89:
-			table[i] = True
-			break
-		if number == 1:
-			table[i] = False
+			square_set.add(i)
 			break
 
 		number = square_sum(number)
 
 
 for i in range(1, 10**7+1):
-	count += table[square_sum(i)]
+	if square_sum(i) in square_set:
+		count += 1
 	if (i % 10**4) == 0:
 		print (i)
 
